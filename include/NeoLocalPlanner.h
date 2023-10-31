79e53021ee5b35bcabe07ee67ac9aaa8728bcbc9
Ivan Villaverde De La Nava <ivan.villaverde@tecnalia.com>
2023-10-31 14:20:06 +0100

    Remove hardcoded global frame and make namespaces optional

--- include/NeoLocalPlanner.h
+++ include/NeoLocalPlanner.h
@@ -57,190 +57,191 @@
 #include "geometry_msgs/msg/pose2_d.hpp"
 #include "geometry_msgs/msg/vector3_stamped.hpp"
 
+namespace neo_local_planner
+{
 
-namespace neo_local_planner {
+	class NeoLocalPlanner : public nav2_core::Controller
+	{
 
-class NeoLocalPlanner : public nav2_core::Controller {
+	public:
+		/**
+		 * @brief Constructor for nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
+		 */
+		NeoLocalPlanner() = default;
 
-public:
-  /**
-   * @brief Constructor for nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
-   */
-  NeoLocalPlanner() = default;
+		/**
+		 * @brief Destrructor for nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
+		 */
+		~NeoLocalPlanner() override = default;
 
-  /**
-   * @brief Destrructor for nav2_regulated_pure_pursuit_controller::RegulatedPurePursuitController
-   */
-  ~NeoLocalPlanner() override = default;
+		/**
+		 * @brief Configure controller state machine
+		 * @param parent WeakPtr to node
+		 * @param name Name of plugin
+		 * @param tf TF buffer
+		 * @param costmap_ros Costmap2DROS object of environment
+		 */
+		void configure(
+			const rclcpp_lifecycle::LifecycleNode::WeakPtr &parent,
+			std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
+			const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
 
-  /**
-   * @brief Configure controller state machine
-   * @param parent WeakPtr to node
-   * @param name Name of plugin
-   * @param tf TF buffer
-   * @param costmap_ros Costmap2DROS object of environment
-   */
-  void configure(
-    const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
-    std::string name, const std::shared_ptr<tf2_ros::Buffer> tf,
-    const std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros) override;
+		/**
+		 * @brief Cleanup controller state machine
+		 */
+		void cleanup() override;
 
-  /**
-   * @brief Cleanup controller state machine
-   */
-  void cleanup() override;
+		/**
+		 * @brief Activate controller state machine
+		 */
+		void activate() override;
 
-  /**
-   * @brief Activate controller state machine
-   */
-  void activate() override;
+		/**
+		 * @brief Deactivate controller state machine
+		 */
+		void deactivate() override;
 
-  /**
-   * @brief Deactivate controller state machine
-   */
-  void deactivate() override;
+		/**
+		 * @brief Compute the best command given the current pose and velocity, with possible debug information
+		 *
+		 * Same as above computeVelocityCommands, but with debug results.
+		 * If the results pointer is not null, additional information about the twists
+		 * evaluated will be in results after the call.
+		 *
+		 * @param pose      Current robot pose
+		 * @param velocity  Current robot velocity
+		 * @param results   Output param, if not NULL, will be filled in with full evaluation results
+		 * @return          Best command
+		 */
+		geometry_msgs::msg::TwistStamped computeVelocityCommands(
+			const geometry_msgs::msg::PoseStamped &pose,
+			const geometry_msgs::msg::Twist &speed,
+			nav2_core::GoalChecker *goal_checker) override;
 
-  /**
-   * @brief Compute the best command given the current pose and velocity, with possible debug information
-   *
-   * Same as above computeVelocityCommands, but with debug results.
-   * If the results pointer is not null, additional information about the twists
-   * evaluated will be in results after the call.
-   *
-   * @param pose      Current robot pose
-   * @param velocity  Current robot velocity
-   * @param results   Output param, if not NULL, will be filled in with full evaluation results
-   * @return          Best command
-   */
-  geometry_msgs::msg::TwistStamped computeVelocityCommands(
-    const geometry_msgs::msg::PoseStamped & pose,
-    const geometry_msgs::msg::Twist & speed,
-    nav2_core::GoalChecker * goal_checker) override;
+		/**
+		 * @brief nav2_core setPlan - Sets the global plan
+		 * @param path The global plan
+		 */
+		void setPlan(const nav_msgs::msg::Path &path) override;
 
-  /**
-   * @brief nav2_core setPlan - Sets the global plan
-   * @param path The global plan
-   */
-  void setPlan(const nav_msgs::msg::Path & path) override;
+		/**
+		 * @brief Limits the maximum linear speed of the robot.
+		 * @param speed_limit expressed in absolute value (in m/s)
+		 * or in percentage from maximum robot speed.
+		 * @param percentage Setting speed limit in percentage if true
+		 * or in absolute values in false case.
+		 */
+		void setSpeedLimit(const double &speed_limit, const bool &percentage) override;
 
-  /**
-   * @brief Limits the maximum linear speed of the robot.
-   * @param speed_limit expressed in absolute value (in m/s)
-   * or in percentage from maximum robot speed.
-   * @param percentage Setting speed limit in percentage if true
-   * or in absolute values in false case.
-   */
-  void setSpeedLimit(const double & speed_limit, const bool & percentage) override;
+		/**
+		 * @brief Callback executed when a parameter change is detected
+		 * @param event ParameterEvent message
+		 */
+		rcl_interfaces::msg::SetParametersResult
+		dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters);
 
-  /**
-   * @brief Callback executed when a parameter change is detected
-   * @param event ParameterEvent message
-   */
-  rcl_interfaces::msg::SetParametersResult
-  dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters);
+		void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
 
-	void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+		bool reset_lastvel(nav_msgs::msg::Path m_global_plan, nav_msgs::msg::Path plan);
 
-	bool reset_lastvel(nav_msgs::msg::Path m_global_plan, nav_msgs::msg::Path plan);
+	private:
+		std::shared_ptr<tf2_ros::Buffer> tf_;
+		std::string plugin_name_;
+		std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
+		nav2_costmap_2d::Costmap2D *costmap_;
+		rclcpp::Logger logger_{rclcpp::get_logger("NeoLocalPlanner")};
+		tf2_ros::Buffer *m_tf = 0;
+		nav2_costmap_2d::Costmap2DROS *m_cost_map;
+		nav_msgs::msg::Path m_global_plan;
+		rclcpp::Clock::SharedPtr clock_;
 
-private:
-	std::shared_ptr<tf2_ros::Buffer> tf_;
-	std::string plugin_name_;
-	std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_;
-	nav2_costmap_2d::Costmap2D * costmap_;
-	rclcpp::Logger logger_ {rclcpp::get_logger("NeoLocalPlanner")};
-	tf2_ros::Buffer* m_tf = 0;
-	nav2_costmap_2d::Costmap2DROS* m_cost_map;
-	nav_msgs::msg::Path m_global_plan;
-	rclcpp::Clock::SharedPtr clock_;
+		std::mutex m_mutex;
+		nav_msgs::msg::Odometry::SharedPtr m_odometry;
 
-	std::mutex m_mutex;
-	nav_msgs::msg::Odometry::SharedPtr m_odometry;
+		rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr m_odom_sub;
+		rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
+		rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
+		std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> m_local_plan_pub;
 
-	rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr m_odom_sub;
-  rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
-	rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
-  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> m_local_plan_pub;
+		std::string m_global_frame;
+		std::string m_local_frame;
+		std::string m_base_frame;
 
-	std::string m_global_frame = "map";
-	std::string m_local_frame;
-	std::string m_base_frame;
+		enum state_t
+		{
+			STATE_IDLE,
+			STATE_TRANSLATING,
+			STATE_ROTATING,
+			STATE_ADJUSTING,
+			STATE_TURNING,
+			STATE_STUCK
+		};
 
-	enum state_t {
-		STATE_IDLE,
-		STATE_TRANSLATING,
-		STATE_ROTATING,
-		STATE_ADJUSTING,
-		STATE_TURNING,
-		STATE_STUCK
+		state_t m_state = state_t::STATE_IDLE;
+
+		rclcpp::Time m_last_time;
+		rclcpp::Time m_first_goal_reached_time;
+
+		bool m_is_goal_reached = false;
+		uint64_t m_update_counter = 0;
+		double m_last_control_values[3] = {};
+		geometry_msgs::msg::Twist m_last_cmd_vel;
+
+	protected:
+		double acc_lim_x = 0;
+		double acc_lim_y = 0;
+		double acc_lim_theta = 0;
+		double acc_lim_trans = 0;
+		double min_vel_x = 0;
+		double max_vel_x = 0;
+		double min_vel_y = 0;
+		double max_vel_y = 0;
+		double min_rot_vel = 0;
+		double max_rot_vel = 0;
+		double min_trans_vel = 0;
+		double max_trans_vel = 0;
+		double min_vel_theta = 0;
+		double max_vel_theta = 0;
+		double min_vel_trans = 0;
+		double max_vel_trans = 0;
+		double rot_stopped_vel = 0;
+		double theta_stopped_vel = 0;
+		double trans_stopped_vel = 0;
+		double yaw_goal_tolerance = 0;
+		double xy_goal_tolerance = 0;
+		double goal_tune_time = 0;
+		bool differential_drive = false;
+		bool constrain_final = false;
+		double start_yaw_error = 0.0;
+		double lookahead_time = 0.0;
+		double m_lookahead_dist = 0.0;
+		double pos_x_gain = 0.0;
+		double pos_y_gain = 0.0;
+		double pos_y_yaw_gain = 0.0;
+		double yaw_gain = 0.0;
+		double static_yaw_gain = 0.0;
+		double cost_x_gain = 0.0;
+		double cost_y_gain = 0.0;
+		double cost_y_yaw_gain = 0.0;
+		double m_cost_y_lookahead_dist = 0.0;
+		double cost_y_lookahead_time = 0.0;
+		double cost_yaw_gain = 0.0;
+		double low_pass_gain = 0.0;
+		double max_cost = 0.0;
+		double max_curve_vel = 0.0;
+		double max_goal_dist = 0.0;
+		double max_backup_dist = 0.0;
+		double min_stop_dist = 0.0;
+		double emergency_acc_lim_x = 0.0;
+		bool enable_software_stop = false;
+		bool m_reset_lastvel = false;
+		bool m_allow_reversing = false;
+		bool use_namespace = false;
+		double m_robot_direction = 1.0;
+		std::string odom_topic = "odom";
+		std::string local_plan_topic = "local_plan";
+		int count = 0;
 	};
-
-	state_t m_state = state_t::STATE_IDLE;
-
-	rclcpp::Time m_last_time;
-	rclcpp::Time m_first_goal_reached_time;
-
-	bool m_is_goal_reached = false;
-	uint64_t m_update_counter = 0;
-	double m_last_control_values[3] = {};
-	geometry_msgs::msg::Twist m_last_cmd_vel;
-
-protected:
-	double acc_lim_x = 0;
-	double acc_lim_y = 0;
-	double acc_lim_theta = 0;
-	double acc_lim_trans = 0;
-	double min_vel_x = 0;
-	double max_vel_x = 0;
-	double min_vel_y = 0;
-	double max_vel_y = 0;
-	double min_rot_vel = 0;
-	double max_rot_vel = 0;
-	double min_trans_vel = 0;
-	double max_trans_vel = 0;
-	double min_vel_theta = 0;
-	double max_vel_theta = 0;
-	double min_vel_trans = 0;
-	double max_vel_trans = 0;
-	double rot_stopped_vel = 0; 
-	double theta_stopped_vel = 0;
-	double trans_stopped_vel = 0;
-	double yaw_goal_tolerance = 0;
-	double xy_goal_tolerance = 0;
-	double goal_tune_time = 0;
-	bool differential_drive = false;
-	bool constrain_final = false;
-	double start_yaw_error = 0.0;
-	double lookahead_time = 0.0;
-	double m_lookahead_dist = 0.0;
-	double pos_x_gain = 0.0;
-	double pos_y_gain = 0.0;
-	double pos_y_yaw_gain = 0.0;
-	double yaw_gain = 0.0;
-	double static_yaw_gain = 0.0;
-	double cost_x_gain = 0.0;
-	double cost_y_gain = 0.0;
-	double cost_y_yaw_gain = 0.0;
-	double m_cost_y_lookahead_dist = 0.0;
-	double cost_y_lookahead_time = 0.0;
-	double cost_yaw_gain = 0.0;
-	double low_pass_gain = 0.0;
-	double max_cost = 0.0;
-	double max_curve_vel = 0.0;
-	double max_goal_dist = 0.0;
-	double max_backup_dist = 0.0;
-	double min_stop_dist = 0.0;
-	double emergency_acc_lim_x = 0.0;
-	bool enable_software_stop = false;
-	bool m_reset_lastvel = false;
-	bool m_allow_reversing = false;
-	double m_robot_direction = 1.0;
-	std::string odom_topic = "odom";
-	std::string local_plan_topic = "local_plan";
-	int count = 0;
-	
-};
-
 
 } // neo_local_planner
 
